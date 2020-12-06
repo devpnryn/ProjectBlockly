3632d957b6fd5d45b41631b19437b814ff50bc04
Narayana <imchinna@Narayanas-MacBook-Air.local>
2020-12-07 00:20:16 +0530

    Just formatted using prettier

--- scripts/customBlocks.js
+++ scripts/customBlocks.js
@@ -1,67 +1,68 @@
-Blockly.defineBlocksWithJsonArray([{
-    "type": "robot_position",
-    "message0": "X %1 %2 Y %3 %4 Z %5 %6 E %7",
-    "args0": [
+Blockly.defineBlocksWithJsonArray([
+  {
+    type: "robot_position",
+    message0: "X %1 %2 Y %3 %4 Z %5 %6 E %7",
+    args0: [
       {
-        "type": "field_number",
-        "name": "X",
-        "value": 0
+        type: "field_number",
+        name: "X",
+        value: 0,
       },
       {
-        "type": "input_dummy"
+        type: "input_dummy",
       },
       {
-        "type": "field_number",
-        "name": "Y",
-        "value": 0
+        type: "field_number",
+        name: "Y",
+        value: 0,
       },
       {
-        "type": "input_dummy"
+        type: "input_dummy",
       },
       {
-        "type": "field_number",
-        "name": "Z",
-        "value": 0
+        type: "field_number",
+        name: "Z",
+        value: 0,
       },
       {
-        "type": "input_dummy"
+        type: "input_dummy",
       },
       {
-        "type": "field_number",
-        "name": "E",
-        "value": 0
-      }
+        type: "field_number",
+        name: "E",
+        value: 0,
+      },
     ],
-    "inputsInline": true,
-    "output": null,
-    "colour": 260,
-    "tooltip": "",
-    "helpUrl": ""
+    inputsInline: true,
+    output: null,
+    colour: 260,
+    tooltip: "",
+    helpUrl: "",
   },
   {
-    "type": "robot_move",
-    "message0": "Move to: %1",
-    "args0": [
+    type: "robot_move",
+    message0: "Move to: %1",
+    args0: [
       {
-        "type": "input_value",
-        "name": "position",
-        "check": "robot_position"
-      }
+        type: "input_value",
+        name: "position",
+        check: "robot_position",
+      },
     ],
-    "previousStatement": null,
-    "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
-    "helpUrl": ""
-  }])
+    previousStatement: null,
+    nextStatement: null,
+    colour: 230,
+    tooltip: "",
+    helpUrl: "",
+  },
+]);
 
-  Blockly.JavaScript['robot_position'] = function(block) {
-    var number_x = block.getFieldValue('X');
-    var number_y = block.getFieldValue('Y');
-    var number_z = block.getFieldValue('Z');
-    var number_e = block.getFieldValue('E');
-    // A word 'GCode:' is added in front of every GCode generation as an identifier
-    var code = `Gcode: G1 X${number_x} Y${number_y} Z${number_z} E${number_e}`
-    return [code, Blockly.JavaScript.ORDER_NONE];
-  };
-  
+Blockly.JavaScript["robot_position"] = function (block) {
+  var number_x = block.getFieldValue("X");
+  var number_y = block.getFieldValue("Y");
+  var number_z = block.getFieldValue("Z");
+  var number_e = block.getFieldValue("E");
+  // A word 'GCode:' is added in front of every GCode generation as an identifier
+  var code = `Gcode: G1 X${number_x} Y${number_y} Z${number_z} E${number_e}`;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
--- src/app.js
+++ src/app.js
@@ -1,82 +1,79 @@
-
 // Configure the Blockly into given workspace
-let demoWorkspace = Blockly.inject('blocklyDiv',
-{media: '../media/',
- toolbox: document.getElementById('toolbox')});
-Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'),
-                       demoWorkspace);
+let demoWorkspace = Blockly.inject("blocklyDiv", {
+  media: "../media/",
+  toolbox: document.getElementById("toolbox"),
+});
+Blockly.Xml.domToWorkspace(
+  document.getElementById("startBlocks"),
+  demoWorkspace
+);
 
 // User can see the Blockly code in Javascript
-const showCode=()=> {
-// Generate JavaScript code and display it.
-Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
-let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
-let sanitizedCode = code.replace(/GCode:/i,'')
-alert(sanitizedCode);
-}
+const showCode = () => {
+  // Generate JavaScript code and display it.
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+  let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+  let sanitizedCode = code.replace(/GCode:/i, "");
+  alert(sanitizedCode);
+};
 
 // User can see the GCode(s) of translated Javascript logic
-const generateGCode=()=>{
-      // Generate JavaScript code and display it.
-Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
-let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
-alert(extractGCodes(code))
-}
+const generateGCode = () => {
+  // Generate JavaScript code and display it.
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+  let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+  alert(extractGCodes(code));
+};
 
 // User can save the GCode(s) of translated Javascript logic into a file
 // A file named GCodes.txt gets created and downloaded to Browser's default path
-const SaveGCodes=()=>{
-Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
-let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
-let gcodes = extractGCodes(code)
-if(gcodes.length === 0) return
- saveTextAsFile(gcodes.join('\n'))
-}
+const SaveGCodes = () => {
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+  let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+  let gcodes = extractGCodes(code);
+  if (gcodes.length === 0) return;
+  saveTextAsFile(gcodes.join("\n"));
+};
 
 // Extracts GCode string(s) from the given code
-const extractGCodes=(code) =>{
-// Look out for word 'GCode' in the translated Javascript code
-const regex = /Gcode:(.*)/gm;
-let m;
-let gStream = []
-while ((m = regex.exec(code)) !== null) {
-// This is necessary to avoid infinite loops with zero-width matches
-if (m.index === regex.lastIndex) {
-    regex.lastIndex++;
-}
-if (m === null)
-    return;
-let gCode = m[1].replace(/[),;,']/g, '').trim()
-gStream.push(gCode)
-}
-return gStream
-}
-
-// a helper function to save data to a file 
-const saveTextAsFile=(gCodes)=>
-{
-    let textToWrite = gCodes
-    let textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-    let fileNameToSaveAs = "GCodes.txt";
-      let downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "Download File";
-    if (window.webkitURL != null)
-    {
-        // Chrome allows the link to be clicked
-        // without actually adding it to the DOM.
-        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+const extractGCodes = (code) => {
+  // Look out for word 'GCode' in the translated Javascript code
+  const regex = /Gcode:(.*)/gm;
+  let m;
+  let gStream = [];
+  while ((m = regex.exec(code)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
     }
-    else
-    {
-        // Firefox requires the link to be added to the DOM
-        // before it can be clicked.
-        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-        downloadLink.onclick = destroyClickedElement;
-        downloadLink.style.display = "none";
-        document.body.appendChild(downloadLink);
-    }
+    if (m === null) return;
+    let gCode = m[1].replace(/[),;,']/g, "").trim();
+    gStream.push(gCode);
+  }
+  return gStream;
+};
 
-    downloadLink.click();
-    document.body.removeChild(downloadLink)
-}
+// a helper function to save data to a file
+const saveTextAsFile = (gCodes) => {
+  let textToWrite = gCodes;
+  let textFileAsBlob = new Blob([textToWrite], { type: "text/plain" });
+  let fileNameToSaveAs = "GCodes.txt";
+  let downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL != null) {
+    // Chrome allows the link to be clicked
+    // without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM
+    // before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+};
